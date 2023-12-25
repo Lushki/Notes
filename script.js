f2bf2239eef31518f2a98356f59ab3e7f4a5759a
Lushki <eladhelfman@gmail.com>
2023-12-25 18:52:08 +0200

    Notes with delet buttons <no function on delet yet>

--- script.js
+++ script.js
@@ -4,22 +4,46 @@ function addNote(event)
 {
         event.preventDefault();
         let newNote = 
-        {
-                "id" : id,
-                content : document.getElementById('newNote').value
-        };
+                {
+                        "id" : id,
+                        content : document.getElementById('newNote').value
+                };
         id += 1;
-                notes.push(newNote);
-        document.getElementById('content').innerHTML = notes.map(note => note.content);
+        notes.push(newNote);
+        document.getElementById('newNote').value = '';
+        displayNotes();
 
-        console.log(notes);
+
+        console.log(notes); //REMOVE LATER
+}
+
+function deletNote()
+{
+        
 }
 
 function displayNotes()
 {
+        const container = document.createElement('content');
+        container.innerHTML = '';
         notes.forEach(note => {
-                
+                const  noteElement = document.createElement('div')
+                noteElement.innerHTML = 
+                `<form class="note" id="note-${note.id}">
+                Note: <span class="content">${note.content}</span>
+                <button class="removeButton" type="button">Remove</button>
+            </form>`
+
+            container.appendChild(noteElement); 
         });
+
+        //Clearing the container before looping through the notes so as to not duplicate notes
+        const contentDiv = document.getElementById('content');
+        contentDiv.innerHTML = '';
+        contentDiv.appendChild(container);
+        
 }
+
+
 
 document.getElementById('noteForm').addEventListener('submit',addNote);
