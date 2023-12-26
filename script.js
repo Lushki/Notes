d8adb169636892b8ec6c8774c64dd3bbc7e446d8
Lushki <eladhelfman@gmail.com>
2023-12-26 15:03:32 +0200

    Added serch bar to index, Added a filter to notes befor displaying.

--- script.js
+++ script.js
@@ -1,80 +1,81 @@
-let notes =[];
+let notes = [];
 let id = 0;
+let filter = '';
 
 function initialize() {
-        const storedNotes = localStorage.getItem('notes');
-        if (storedNotes) {
-            notes = JSON.parse(storedNotes);
-            id = notes.length;
-            displayNotes();
-        }
+    // Loads the msgs from localStorage on load
+    const storedNotes = localStorage.getItem('notes');
+    if (storedNotes) {
+        notes = JSON.parse(storedNotes);
+        id = notes.length;
+        displayNotes();
     }
-
-function addNote(event)
-{
-        event.preventDefault();
-        let newNote = 
-                {
-                        "id" : id,
-                        content : document.getElementById('newNote').value
-                };
-        id += 1;
-        notes.push(newNote);
-        document.getElementById('newNote').value = '';
-        displayNotes();
-        updateNoteStorage();
-        
-        console.log(notes); //REMOVE LATER
 }
 
-function deletNote(noteId)
-{ 
-        //Querrs all notes and leaves out the one with the rec id
-        notes = notes.filter(note => note.id !== noteId); 
-        displayNotes();
-        updateNoteStorage();
-        console.log(notes); //REMOVE LATER
+function addNote(event) {
+    event.preventDefault();
+    let newNote = {
+        "id": id,
+        content: document.getElementById('newNote').value
+    };
+    id += 1;
+    notes.push(newNote);
+    document.getElementById('newNote').value = '';
+    displayNotes();
+    updateNoteStorage();
+    console.log(notes); // REMOVE LATER
 }
 
-function displayNotes()
-{
-        const container = document.createElement('div');
-        container.innerHTML = '';
-        notes.forEach(note => {
-                const  noteElement = document.createElement('div')
-                noteElement.innerHTML = 
+function deleteNote(noteId) {
+    // Queries all notes and leaves out the one with the received id
+    notes = notes.filter(note => note.id !== noteId);
+    displayNotes();
+    updateNoteStorage();
+    console.log(notes); // REMOVE LATER
+}
+
+function displayNotes() {
+    const container = document.createElement('div');
+    container.innerHTML = '';
+    notes.forEach(note => {
+        if (note.content.includes(filter)) {
+            const noteElement = document.createElement('div');
+            noteElement.innerHTML =
                 `<form class="note" id="note-${note.id}">
-                Note: <span class="content">${note.content}</span>
-                <button class="removeButton" type="button" onclick="deletNote(${note.id})">Remove</button>
-                <button class="editButton" type="button" onclick="editNote(${note.id})">Edit</button>
-                </form>`
+                    Note: <span class="content">${note.content}</span>
+                    <button class="removeButton" type="button" onclick="deleteNote(${note.id})">Remove</button>
+                    <button class="editButton" type="button" onclick="editNote(${note.id})">Edit</button>
+                </form>`;
+            container.appendChild(noteElement);
+        }
+    });
 
-            container.appendChild(noteElement); 
-        });
-
-        //Clearing the container before looping through the notes so as to not duplicate notes
-        const contentDiv = document.getElementById('content');
-        contentDiv.innerHTML = '';
-        contentDiv.appendChild(container);
-        
+    const contentDiv = document.getElementById('content');
+    contentDiv.innerHTML = '';
+    contentDiv.appendChild(container);
 }
 
-function editNote(noteId)
-{
-        const noteToEdit = notes.find(note => note.id === noteId);
-        const updatedContent = prompt('Edit note:', noteToEdit.content);
-        if (updatedContent !== null) {
-                noteToEdit.content = updatedContent;
-                displayNotes();
-                updateNoteStorage();
-            }
-        console.log(notes); //Remove Later
+function editNote(noteId) {
+    const noteToEdit = notes.find(note => note.id === noteId);
+    const updatedContent = prompt('Edit note:', noteToEdit.content);
+    if (updatedContent !== null) {
+        noteToEdit.content = updatedContent;
+        displayNotes();
+        updateNoteStorage();
+    }
+    console.log(notes); // Remove Later
 }
 
-function updateNoteStorage()
-{
-        localStorage.setItem('notes',JSON.stringify(notes));
+function updateNoteStorage() {
+    localStorage.setItem('notes', JSON.stringify(notes));
 }
 
-window.addEventListener('load',initialize);
-document.getElementById('noteForm').addEventListener('submit',addNote);
+function setFilter(event) {
+    event.preventDefault();
+    filter = document.getElementById('searchBar').value;
+    displayNotes();
+}
+
+window.addEventListener('load', initialize);
+document.getElementById('noteForm').addEventListener('submit', addNote);
+document.getElementById('filterForm').addEventListener('submit', setFilter);
--- structure.md
+++ structure.md
@@ -13,5 +13,8 @@ https://www.youtube.com/watch?v=k8yJCeuP6I8&ab_channel=dcode
 validation of notes: *note cant be empty [V] -just added required to content
 
 # Addisionaly:
-filtering of the notes by: text included.
-text formating inside the notes: text markdown; Bold, Underline, Size and Font. 
+filtering of the notes by: content. [V] 
+https://stackoverflow.com/questions/35235794/filter-strings-in-array-based-on-content-filter-search-value
+
+text formating inside the notes: text markdown; Bold, Underline, Size and Font. []
+*if css stored as content it will display when i edit.
